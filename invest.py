a4accebbfd203989d2be2e160f48b23248fe40fc
atinyz370 <148353692+atinyz370@users.noreply.github.com>
2023-11-06 00:21:15 +0700

    Add files via upload
    update period

--- invest.py
+++ invest.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 st.title('Buy or Sell')
 
-watchlist = ['AH.BK','SPVI.BK','MEGA.BK','TISCO.BK','UTP.BK','NTES','AON','other']
+watchlist = ['AH.BK','SPVI.BK','MEGA.BK','TISCO.BK','NTES','AON','other']
 my_watchlist = st.selectbox("choose your watchlist", watchlist)
 if my_watchlist == 'other':
     my_ticker = st.text_input('Type ticker')
@@ -17,10 +17,11 @@ else:
     my_ticker = my_watchlist
 
 d_start = st.date_input("Select start date", value=None, format="MM-DD-YYYY", key=1)
-d_end = st.date_input("Select start date", value=None, format="MM-DD-YYYY", key=2)
+d_end = st.date_input("Select end date", value=None, format="MM-DD-YYYY", key=2)
+d_period = st.number_input("Period", min_value=30, max_value=180, value=30, placeholder="forcasting period")
 
-st.write('start date:', d_start)
-st.write('end date:', d_end)
+# st.write('start date:', d_start)
+# st.write('end date:', d_end)
 
 if my_ticker =="" :
     pass
@@ -32,7 +33,7 @@ else:
 
     m = Prophet()
     m.fit(prophet_df) #train data
-    future = m.make_future_dataframe(periods=30) #future period
+    future = m.make_future_dataframe(periods=d_period) #future period
     forecast = m.predict(future) 
 
     px.line(forecast, x='ds', y='yhat')
